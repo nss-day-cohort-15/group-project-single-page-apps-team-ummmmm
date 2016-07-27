287e31973d1266bbf5d8a1f21f8f9c1e66ddf3b5
Casey Reed <casey.w.reed@gmail.com>
2016-07-27 15:35:08 -0500

    Refixed username display on initial load

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showMessage(message){
    var message_div = document.querySelector("#userMessageDisplay");
    message.forEach(function(content){
-   message_div.innerHTML += `<p class='message'>${content.message}
+   message_div.innerHTML += `<p class='message'>${content.user}: ${content.message}
                      <button class='remove'>delete</button></p>`;
   })
 }
