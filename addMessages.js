aa5e140b1901097b4a6548a0009643f7cf065d92
Casey Reed <casey.w.reed@gmail.com>
2016-07-27 18:29:31 -0500

    Switched around some IIFE stuff. Still a working MVP

--- addMessages.js
+++ addMessages.js
@@ -2,14 +2,17 @@
 document.addEventListener("keydown", checkForEnter);
 function checkForEnter(e) {
   if (e.keyCode == 13) {
-     addMessage(e)
+    username = document.getElementById("usernameInput").value
+    userMessage = document.getElementById("userMessageInput").value
+    Chatty.addMessage(username, userMessage)
   }
 }
 
-function addMessage (e) {
-  username = document.getElementById("usernameInput").value
-  userMessage = document.getElementById("userMessageInput").value
-  document.getElementById("userMessageDisplay").innerHTML += `<p class='message'>${username}: ${userMessage}
-                     <button class='remove'>delete</button></p>`;
-}
+// function addMessage (e) {
+//   username = document.getElementById("usernameInput").value
+//   userMessage = document.getElementById("userMessageInput").value
+//   document.getElementById("userMessageDisplay").innerHTML += `<p class='message'>${username}: ${userMessage}
+//                      <button class='remove'>delete</button></p>`;
+// }
 
+//USE A COUNTER VARIABLE. CHECK WHETHER THE COUNTER IS EVEN OR ODD TO COLOR THE BACKGROUND LIGHT GREY OR WHITE
--- chatty.js
+++ chatty.js
@@ -6,7 +6,7 @@ function loadFirstMessages () {
   xhr.open('GET', 'messages.json');
   xhr.addEventListener('load', function(event){
   _message = JSON.parse(event.target.responseText);
-  document.getElementById("userMessageDisplay").innerHTML = _message
+  document.getElementById("userMessageDisplay").innerHTML = _message //Couldn't we use a callback function to show the messages w/ styling here?
     })
  }
 
@@ -19,7 +19,21 @@ function loadFirstMessages () {
         callback(_message);
       })
       xhr.send();
-    }
+    },
+
+    addMessage: function (username, userMessage) {
+      // username = document.getElementById("usernameInput").value
+      // userMessage = document.getElementById("userMessageInput").value
+      document.getElementById("userMessageDisplay").innerHTML +=
+        `<p class='message'><span class='boldUser'>${username}:</span> ${userMessage}<button class='remove'>delete</button></p>`;
+    },
+    showMessage: function (message){
+      var message_div = document.querySelector("#userMessageDisplay");
+      message.forEach(function(content){
+      message_div.innerHTML += `<p class='message'>${content.user}: ${content.message}
+                       <button class='remove'>delete</button></p>`;
+  })
+}
   }
 
 
