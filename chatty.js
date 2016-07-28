7492bc2e5008a3cff917fdc1781f6af544e6b056
Casey Reed <casey.w.reed@gmail.com>
2016-07-28 11:18:08 -0500

    Completely fixed IIFE structure.

--- chatty.js
+++ chatty.js
@@ -1,8 +1,6 @@
 var Chatty = (function (){
   var _message = [];
 
-
-
   return {
     getMessage: function(callback){
       var xhr = new XMLHttpRequest();
@@ -17,37 +15,19 @@ var Chatty = (function (){
     addMessage: function (username, userMessage) {
       userMessageObject = {"user": username, "message": userMessage}
       _message.push(userMessageObject)
-      console.log(_message)
+      Chatty.showMessage(_message)
       },
-
-    // addMessage: function (username, userMessage) {
-    // _message.push({"user": username, "message": userMessage})
-    // showMessage(_message)
-    //     },
 
     showMessage: function (message){
       var message_div = document.querySelector("#userMessageDisplay");
+      message_div.innerHTML = ""
+
       message.forEach(function(content){
-      message_div.innerHTML += `<p class='message'><span class='boldUser'>${content.user}:</span> ${content.message}
+      message_div.innerHTML +=  `<p class='message'><span class='boldUser'>${content.user}:</span> ${content.message}
                        <button class='remove'>delete</button></p>`;
   })
 },
-
-
-
   }
 }())
 
 Chatty.getMessage(Chatty.showMessage);
-
-// addMessage: function (username, userMessage) {
-// _message.push(`<p class='message'><span class='boldUser'>${username}:</span> ${userMessage}<button class='remove'>delete</button></p>`)
-// document.getElementById("userMessageDisplay").innerHTML = _message;
-//   },
-
-
-// addMessage: function (username, userMessage) {
-//   userMessageObject = {"user": username, "message": userMessage}
-//   document.getElementById("userMessageDisplay").innerHTML +=
-//     `<p class='message'><span class='boldUser'>${username}:</span> ${userMessage}<button class='remove'>delete</button></p>`;
-//   },
